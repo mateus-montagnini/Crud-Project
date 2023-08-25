0792bf5fd0585e9bdb9baa14b88a9c0e2cd861e1
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-25 17:31:45 -0300

    Record RequestProduct

---
+++ Crud-Project/src/main/java/com/mrocha/crud/domain/product/RequestProduct.java
@@ -0,0 +1,11 @@
+package com.mrocha.crud.domain.product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RequestProduct(
+        String id,
+        @NotBlank String name,
+        @NotNull Integer price_in_cents) {
+
+}
