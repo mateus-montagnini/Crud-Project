cddfb74758590cd32cc088a04a23a623eb07c495
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-26 13:30:38 -0300

    Classe Classe Product atualizada

--- Crud-Project/src/main/java/com/mrocha/crud/domain/product/Product.java
+++ Crud-Project/src/main/java/com/mrocha/crud/domain/product/Product.java
@@ -3,6 +3,8 @@ package com.mrocha.crud.domain.product;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.UUID;
+
 @Table(name = "product")
 @Entity(name = "product")
 @Getter
@@ -17,10 +19,12 @@ public class Product {
     private String id;
     private String name;
     private Integer price_in_cents;
+    private Boolean active;
 
     public Product(RequestProduct requestProduct) {
         this.name = requestProduct.name();
         this.price_in_cents = requestProduct.price_in_cents();
+        this.active = true;
     }
 
 }
