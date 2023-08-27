fc8afea1f4674570716af1770358de05db33f2fd
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-27 11:44:15 -0300

    Criando RequestsExceptionHandler

---
+++ Crud-Project/src/main/java/com/mrocha/crud/infra/RequestsExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.mrocha.crud.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestsExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404() {
+//        var response = new ExceptionDTO("Dado nao encontrado");
+        return ResponseEntity.badRequest().body("Dado nao encontrado");
+    }
+}
